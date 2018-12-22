ce33be7f96640aad390a66a8093fab56e3f1c8bd
Mike Dettmer <mike@mikedettmer.com>
2018-12-21 21:49:44 -0500

    feat() - generalizes server and adds placeholder index.js

--- bin/server.js
+++ bin/server.js
@@ -2,6 +2,8 @@ require('babel/register')({
   ignore: /node_modules\/(?!gstv)/
 });
 
+const app = require('../index');
+
 // const mongoose = require('mongoose');
 // const config = require('../config');
 // const app = require('../index');
@@ -15,39 +17,39 @@ require('babel/register')({
 
 // server.timeout = 0;
 
-// // Chain disconnects to ensure completion
-// function chainDisconnect(cb) {
-//   mongoose.disconnect((msg1) => {
-//     if (msg1) console.info('Mongoose Disconnect Step', msg1);
-//     log4js.shutdown((msg4) => {
-//       if (msg4) console.info('Log4JS Disconnect Step', msg4);
-//       cb();
-//     });
-//   });
-// }
+// Chain disconnects to ensure completion
+function chainDisconnect(cb) {
+  // mongoose.disconnect((msg1) => {
+  //   if (msg1) console.info('Mongoose Disconnect Step', msg1);
+  //   log4js.shutdown((msg4) => {
+  //     if (msg4) console.info('Log4JS Disconnect Step', msg4);
+  //     cb();
+  //   });
+  // });
+}
 
-// // Wrap disconnects and pass exit strategy
-// function gracefulShutdown() {
-//   console.info('Graceful Shutdown Initiated');
-//   chainDisconnect(() => {
-//     console.info('Connections Closed: EXITING ', new Date().toISOString());
-//     process.exit();
-//   });
-// }
+// Wrap disconnects and pass exit strategy
+function gracefulShutdown() {
+	console.info('Graceful Shutdown Initiated');
+	chainDisconnect(() => {
+		console.info('Connections Closed: EXITING ', new Date().toISOString());
+		process.exit();
+	});
+}
 
-// // Allow Kubernetes time to hault traffic and connections
-// const READINESS_PROBE_DELAY = 5000;
-// const signals = ['SIGINT', 'SIGTERM'];
+// Allow Kubernetes time to hault traffic and connections
+const READINESS_PROBE_DELAY = 5000;
+const signals = ['SIGINT', 'SIGTERM'];
 
-// // Listen for signals
-// signals.forEach((sig) => {
-//   process.on(sig, () => {
-//     console.info(` Received ${sig} `, new Date().toISOString());
-//     setTimeout(gracefulShutdown, READINESS_PROBE_DELAY);
-//   });
-// });
+// Listen for signals
+signals.forEach((sig) => {
+	process.on(sig, () => {
+		console.info(` Received ${sig} `, new Date().toISOString());
+		setTimeout(gracefulShutdown, READINESS_PROBE_DELAY);
+	});
+});
 
-// // Acknowledge uncaught exceptions
-// process.on('uncaughtException', function (err) {
-//   console.info('Uncaught Exception: ', err.stack || err);
-// });
+// Acknowledge uncaught exceptions
+process.on('uncaughtException', function (err) {
+	console.info('Uncaught Exception: ', err.stack || err);
+});
